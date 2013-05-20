f8963be13aeda0c73d9522bbf65c45ac134e6ce2
Kalee Eversole <kleversole@gmail.com>
2013-05-19 21:04:33 -0400

    Polished about page

--- about.php
+++ about.php
@@ -2,32 +2,38 @@
 <body class="about">
 	<?php include('inc/header.php'); ?>
 	<div class="page-wrap">
-			<div class="kalee">
 		<h2>About</h2>
-	</div>
 	<div class="block blue">
 		<div class="left">
 		<img src="images/about-kalee.png" alt="Photo of Kalee Eversole"/>
-		<p class="special">Howdy</p>
+		<p class="special">Howdy.</p>
 	</div>
 	<div class="right">
 		<h3>
-			I'm Kalee Eversole. I design &amp; develop modern web sites &amp;
+			My name is Kalee Eversole. I design &amp; develop modern web sites &amp;
 			web experiences.
 		</h3>
-		<p>This is a chunk</p>
-		<p>Another chunk</p>
-		<p>Last chunk</p>
-		</div>
+		<p>I love everything about the web. I design and build web sites
+		that are beautiful, useful and simple.</p>
+		<p>I like things better when they aren't sucky, so I build for the web with this in mind.
+	     I do both user interface design and front-end development. There is something magical about doing both.
+	     I always trust my work is tailor-made from beginning to end. 
+			   </p>
+		<p>Lastly, I am a real human being. I have great husband and the most whimsical, adorable son.
+			I love laughing, good moods, my friends, beer and tacos.</p>
+	</div>
 	</div>
 	<div class="block">
 		<div class="left">
-			<h4>Find Me Here</h4>
-			<h3 class="special">This site is hand-crafted &amp; hand-failed</h3>
-			<a href="#" title="My Process">Let's Talk Process</a>
+			<h4>How This Site Was Made
+				<span>
+					Hand-crafted &amp; Hand-failed
+				</span>
+				</h4>
+			<a class="button small" href="#" title="My Process">See the process that brought this site to life</a>
 		</div>
 		<div class="right">
-			<a href="#" title="Work With Me">Work With Me!</a>
+			<a class="work" href="contact.php" title="Work With Me">Let's Work Together!</a>
 		</div>
 	<?php include('inc/footer.php'); ?>
 	</div><!-- end .page-wrap -->
